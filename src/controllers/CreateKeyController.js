8348490f7d11b37d36eb30ababd278bb0609f80b
Herdeson Mourao <herdesondev@gmail.com>
2022-01-10 23:23:22 -0300

    Feat: :sparkles: criando public e private key e registrando no banco de dados

--- src/controllers/CreateKeyController.js
+++ src/controllers/CreateKeyController.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { generateKeyPair } from "crypto";
+import connMysql from "../config/db_config";
 
 export async function CreateKey(req, res) {
   generateKeyPair(
@@ -18,6 +19,20 @@ export async function CreateKey(req, res) {
       },
     },
     (err, publicKey, privateKey) => {
+
+      const { name } = req.body;
+
+      const register = "INSERT INTO client (name, public_key, private_key) VALUES (?,?,?)";
+
+      const public_key = publicKey;
+      const private_key = privateKey;
+      
+      connMysql.query(register, [
+        name,
+        public_key,
+        private_key,
+      ]);
+
       return res.status(200).json({ publicKey: publicKey, privateKey: privateKey });
     }
   );
