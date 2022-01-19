d98626679e3878c74ace98c2d06a8616cafb1187
Herdeson Mourão <herdesondev@gmail.com>
2022-01-18 22:25:47 -0300

    WIP sign message

--- src/controllers/CreateMessageController.js
+++ src/controllers/CreateMessageController.js
@@ -8,9 +8,9 @@ export async function CreateMessageEncrypted(req, res) {
     const { id_sender, id_recipient, message } = req.body;
     
     const searchIdSender = "SELECT * FROM public_key_communication WHERE id_sender = ? AND id_recipient = ?";
-    //const searchPrivateKey = "SELECT * FROM client WHERE id = ?";
+    const searchPrivateKey = "SELECT * FROM client WHERE id = ?";
     
-    await connMysql.query(searchIdSender, [id_recipient, id_sender], (err, rows) => {
+    await connMysql.query(searchIdSender, [id_recipient, id_sender], async (err, rows) => {
       //mensagem criptografada
       const messageEncrypted = crypto.publicEncrypt(
         {
@@ -21,37 +21,48 @@ export async function CreateMessageEncrypted(req, res) {
         Buffer.from(message)
       );
 
-      //const messageEncryptedBase64 = messageEncrypted.toString('base64');
+      const messageEncryptedBase64 = messageEncrypted.toString('base64');
       
-      // connMysql.query(searchPrivateKey, [id_sender], (err, rowsTwo) => {
-      //   let signer = crypto.createSign('RSA-SHA256');
+      await connMysql.query(searchPrivateKey, [id_sender], (err, rowsTwo) => {
+        // const signature = crypto.sign("sha256", Buffer.from(message), {
+        //   key: rowsTwo[0].private_key,
+        //   padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
+        // })
 
-      //   console.log('1',signer);
+        // console.log('112 => ', signature.toString('base64'));
 
-      //   let x = signer.write(message);
-      //   signer.end()
-      //   console.log('2',x)
+        let signer = crypto.createSign('RSA-SHA256');
+        signer.update(message);
+        console.log(signer.sign(rowsTwo[0].private_key, 'hex'));
 
-      //   let c = signer.sign(rowsTwo[0].private_key, 'base64');
+        // console.log('1',signer);
 
-      //   console.log(c)
-      //   // let a = rowsTwo[0].private_key;
+        // let x = signer.write(message);
+        // signer.end()
+        // console.log('2',x)
 
-      //   // let teste = signer.sign(a, 'base64');
+        // let c = signer.sign(rowsTwo[0].private_key, 'base64');
 
-      //   // console.log('2', teste)
-      // });
+        // console.log(c)
+        // let a = rowsTwo[0].private_key;
 
-      // console.log('1', messageEncrypted);
-      // console.log('2', messageEncrypted.toString("base64"));
+        // let teste = signer.sign(a, 'base64');
+
+        // console.log('2', teste)
+
+        // let mode = crypto.RSA_PKCS1_SHA256;
+        // let message = messageEncrypted;
+        // let key = rowsTwo[0].private_key;
+
+        // crypto.sign(mode, message, key, (err, signature) => {
+        //   console.log('122 = ', signature)
+        // })
+      });
+
+      //console.log('1', messageEncrypted);
+      //console.log('2', messageEncrypted.toString("base64"));
       
-      const senderMessageEncrypted = "INSERT INTO message (id_sender, id_recipient, message) VALUES (?, ?, ?)";
-
-      connMysql.query(senderMessageEncrypted, [
-        id_sender,
-        id_recipient,
-        messageEncrypted
-      ]);
+      
 
       return res.status(200).json({ message: 'mensagem enviada' });
     });
